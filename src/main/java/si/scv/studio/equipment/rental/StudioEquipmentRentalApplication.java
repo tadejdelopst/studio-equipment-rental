1a1269a36ddc5974e7aff41fd3a5adcc31ffd05d
tadejdelopst <tadejdelopst@gmail.com>
2021-03-11 18:19:18 +0100

    Initial project commit

---
+++ src/main/java/si/scv/studio/equipment/rental/StudioEquipmentRentalApplication.java
@@ -0,0 +1,11 @@
+package si.scv.studio.equipment.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioEquipmentRentalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudioEquipmentRentalApplication.class, args);
+    }
+}
